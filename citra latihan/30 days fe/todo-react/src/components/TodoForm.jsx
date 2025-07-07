c6d1bcca2de44e6534d802691606eae44d64d38c
ameliacitra51 <citraamelia25003@gmail.com>
2025-07-07 15:38:00 +0700

    memindahkan struktur folder

--- citra latihan/30 days fe/todo-react/src/components/TodoForm.jsx
+++ citra latihan/30 days fe/todo-react/src/components/TodoForm.jsx
@@ -11,35 +11,35 @@ function TodoForm({addTodo}) {
 
     return (
         <form onSubmit={handleSubmit} style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
-  <input
-    type="text"
-    placeholder="Add Task"
-    value={input}
-    onChange={(e) => setInput(e.target.value)}
-    style={{
-      flex: 1,
-      padding: '10px 14px',
-      fontSize: '16px',
-      borderRadius: '6px',
-      border: '1px solid #ccc',
-      outline: 'none',
-    }}
-  />
-  <button
-    type="submit"
-    style={{
-      backgroundColor: '#685ed8',
-      color: '#fff',
-      padding: '10px 20px',
-      border: 'none',
-      borderRadius: '6px',
-      cursor: 'pointer',
-      fontWeight: 'bold',
-    }}
-  >
-    Add +
-  </button>
-</form>
+            <input
+                type="text"
+                placeholder="Add Task"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                style={{
+                flex: 1,
+                padding: '10px 14px',
+                fontSize: '16px',
+                borderRadius: '6px',
+                border: '1px solid #ccc',
+                outline: 'none',
+                }}
+            />
+            <button
+                type="submit"
+                style={{
+                backgroundColor: '#685ed8',
+                color: '#fff',
+                padding: '10px 20px',
+                border: 'none',
+                borderRadius: '6px',
+                cursor: 'pointer',
+                fontWeight: 'bold',
+                }}
+            >
+                Add +
+            </button>
+        </form>
     );
 }
 
